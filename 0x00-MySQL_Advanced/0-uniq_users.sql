c8b728407686b3497d3ba2e75cbe3d38bd5ac2d6
kwesimain <opokulive@gmail.com>
2024-03-22 02:08:22 +0000

    comm1

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Active: 1695137323808@@127.0.0.1@3306@hbtn_0d_tvshows
+-- a SQL script that creates a table users following requirements
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
